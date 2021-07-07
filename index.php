ab8a26ac507dcf8113f55086877c8905bff84618
Alex <lexuar2000@gmail.com>
2021-07-07 12:05:52 +0300

    Deleted excess info

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
 
-define('STOP_STATISTICS', true);
-require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php';
-$GLOBALS['APPLICATION']->RestartBuffer();
-//header('Content-Type: application/json');
-
-
 class Node
 {
 	public int $value;
